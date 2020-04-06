5b82323c7b1337179a9a2d4521425b639ae60af2
mpubs1b <63019666+mpubs1b@users.noreply.github.com>
2020-04-06 16:28:58 -0400

    Update wp-setup.sh

--- wp-setup.sh
+++ wp-setup.sh
@@ -1,17 +1,23 @@
-### Set the theme name
+### Set the theme names
+PARENT_THEME="underscores-sinai"
 THEME="sinai"
 
 ### Set the local development URL
 LOCALDEV="sinaiboilerplate.local"
 
 ### Set the list of plugins to be activated
-declare -a arr=("autodescription" "advanced-custom-fields" "classic-editor" "ewww-image-optimizer" "wp-asset-clean-up" "autoptimize" )
+declare -a pluginsActivate=("autodescription" "advanced-custom-fields" "classic-editor" "ewww-image-optimizer" "wp-asset-clean-up" "autoptimize" )
 
 ### Set the list of plugins to install but not activate (require further config)
-declare -a twoarr=("two-factor" "easy-wp-smtp" "disable-comments")
+declare -a pluginsForConfig=("two-factor" "easy-wp-smtp" "disable-comments")
 
-wp scaffold _s ${THEME} --activate
-cd wp-content/themes/${THEME}/
+### Set the list of unneeded themes to uninstall
+declare -a arr=("twentynineteen" "twentyseventeen" "twentysixteen" "twentytwenty")
+
+### Create a child theme based on Underscores
+wp scaffold _s ${PARENT_THEME} --theme_name="${PARENT_THEME}"
+
+cd wp-content/themes/${PARENT_THEME}/
 npx wpgulp
 sed -i "" "s/wpgulp.local/${LOCALDEV}/g" wpgulp.config.js
 
@@ -25,16 +31,25 @@ mv js/navigation.js assets/js/custom/navigation.js
 mv js/skip-link-focus-fix.js assets/js/custom/skip-link-focus-fix.js
 rm -R js/
 
-sed -i "" "wp_enqueue_script( \'${THEME}-navigation\'.*/\/\/wp_enqueue_script( '${THEME}-vendor-scripts', get_template_directory_uri() . '/assets/js/vendor.min.js', array(), '20151215', true );/g" functions.php
+sed -i "" "s#wp_enqueue_script( '${PARENT_THEME}-navigation'.*#//wp_enqueue_script( '${THEME}-vendor-scripts', get_template_directory_uri() . '/assets/js/vendor.min.js', array(), '20151215', true );#g" functions.php
 
-sed -i "" "wp_enqueue_script( \'sinai-skip-link-focus-fix.*/wp_enqueue_script( \'${THEME}-theme-custom-scripts\', get_template_directory_uri() . '/assets/js/custom.min.js', array('customize-preview'), '20151215', true );/g" functions.php
+sed -i "" "s#wp_enqueue_script( '${PARENT_THEME}-skip-link-focus-fix.*#wp_enqueue_script( '${THEME}-theme-custom-scripts', get_template_directory_uri() . '/assets/js/custom.min.js', array('customize-preview'), '20151215', true );#g" functions.php
 
-for i in "${arr[@]}"
+sed -i "" "s#imagemin.jpegtran({ progressive: true }),#imagemin.mozjpeg({progressive: true,}),#g" gulpfile.babel.js
+
+npm install 	
+npm install gulp babel-register babel-core @babel/preset-env gulp-sass gulp-uglifycss gulp-autoprefixer gulp-merge-media-queries gulp-rtlcss gulp-concat gulp-uglify gulp-babel @babel/core gulp-imagemin gulp-rename gulp-line-ending-corrector gulp-filter gulp-concat gulp-cache gulp-wp-pot gulp-sourcemaps gulp-remember gulp-rename gulp-sort gulp-plumber gulp-notify beepbeep @babel/preset-env
+npm link gulp gulp-sass gulp-uglifycss gulp-autoprefixer gulp-merge-media-queries gulp-rtlcss gulp-concat gulp-uglify gulp-babel @babel/core gulp-imagemin gulp-rename gulp-line-ending-corrector gulp-filter gulp-concat gulp-cache gulp-wp-pot gulp-sourcemaps gulp-remember gulp-rename gulp-plumber gulp-notify browser-sync beepbeep gulp-sort @babel/preset-env
+npm audit fix
+
+wp scaffold child-theme ${THEME} --parent_theme=${PARENT_THEME} --activate
+
+for i in "${pluginsActivate[@]}"
 do
     wp plugin install $i --activate
 done
  
-for i in "${twoarr[@]}"
+for i in "${pluginsForConfig[@]}"
 do
     wp plugin install $i  
 done
@@ -43,18 +58,6 @@ done
 wp plugin uninstall akismet
 wp plugin uninstall hello
 
-### mkdir wp-content/themes/sage
-### git clone https://github.com/roots/sage.git wp-content/themes/sage
-### wp theme activate sage/resources
-### 
-### cd wp-content/themes/sage
-### npm install && npm audit fix
-### yarn start
-
-### composer create-project roots/sage
-
-### uninstall unneeded themes
-declare -a arr=("twentynineteen" "twentyseventeen" "twentysixteen" "twentytwenty")
 for i in "${arr[@]}"
 do
     wp theme delete $i
